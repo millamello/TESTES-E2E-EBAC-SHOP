f9c8631ef91d105f1db6a7ef5a9ec080c1ccb9d3
Camila Mello <milla_mello@hotmail.com>
2023-03-07 21:19:17 -0300

    Commit atualizado

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/')
+    });
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.adicionarProduto('Atlas Fitness Tank')
+        cy.adicionarProduto2('Ajax Full-Zip Sweatshirt')
+        cy.adicionarProduto3('Arcadio Gym Short')
+        cy.adicionarProduto4('Atomic Endurance Running Tee (Crew-Neck)')
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        cy.EnderecoFaturamento()
+        cy.get('.woocommerce-notice').should('be.visible')
+        
+    })
+    
+
+
+})
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,60 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('adicionarProduto', (produto) => {
+
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.contains(produto).click()
+    cy.get('.button-variable-item-XS').click()
+    cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+    cy.get('.single_add_to_cart_button').click()
+
+})
+
+Cypress.Commands.add('adicionarProduto2', (produto) => {
+
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.contains(produto).click()
+    cy.get('.button-variable-item-XS').click()
+    cy.get('.button-variable-item-Red').click()
+    cy.get('.single_add_to_cart_button').click()
+
+
+})
+
+Cypress.Commands.add('adicionarProduto3', (produto) => {
+
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.contains(produto).click()
+    cy.get('.button-variable-item-32').click()
+    cy.get('.button-variable-item-Red').click()
+    cy.get('.single_add_to_cart_button').click()
+})
+
+Cypress.Commands.add('adicionarProduto4', (produto) => {
+
+    cy.get('#primary-menu > .menu-item-629 > a').click()
+    cy.get(':nth-child(2) > .page-numbers').click()
+    cy.contains(produto).click()
+    cy.get('.button-variable-item-XS').click()
+    cy.get('.button-variable-item-Black').click()
+    cy.get('.single_add_to_cart_button').click()
+})
+
+Cypress.Commands.add('EnderecoFaturamento', (endereco) => {
+
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+        cy.get('#billing_first_name').type('Camila')
+        cy.get('#billing_last_name_field > label').type('Mello')
+        cy.get('#billing_company').type('Tecnoset')
+        cy.get('#billing_country_field > label').click()     
+        cy.get('#billing_address_1').type('Av. Independencia')
+        cy.get('#billing_city').type('Sorocaba')
+        cy.get('#billing_postcode').type('18031-180')
+        cy.get('#billing_phone').type('(15)981518407')
+        cy.get('#billing_email').type('camila@teste.com.br')
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+        
+    })
